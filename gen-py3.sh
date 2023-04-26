d034123f93a0278e591bb3e92d7dc159423ba0d7
tianyuan <tianyuanyuans@163.com>
2023-04-26 17:32:08 +0800

    fix: py3

--- gen-py3.sh
+++ gen-py3.sh
@@ -2,5 +2,6 @@ if [ ! -d "./gen/py3" ]; then
 	mkdir -p "./gen/py3"
 fi
 
-python3 -m grpc_tools.protoc --proto_path=. -I./protos --python_out=./gen/py3 --grpc_python_out=./gen/py3 ./protos/*/*.proto
+# --proto_path=. 可替换成-I.
+python3 -m grpc_tools.protoc --proto_path=. --python_out=./gen/py3 --grpc_python_out=./gen/py3 ./protos/*/*.proto
 echo "proto file create success ..."
