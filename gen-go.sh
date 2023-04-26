56bed226c97b4aa8ce4c9ada6fb245273559ef30
tianyuan <tianyuanyuans@163.com>
2023-04-26 17:27:04 +0800

    feat: py3

--- gen-go.sh
+++ gen-go.sh
@@ -2,5 +2,5 @@ if [ ! -d "./gen/go" ]; then
 	mkdir -p "./gen/go"
 fi
 
-protoc --go_out=./gen/go --go_opt=paths=source_relative --go-grpc_out=./gen/go --go-grpc_opt=paths=source_relative ./protos/*/*.proto
+protoc --proto_path=. --go_out=./gen/go --go_opt=paths=source_relative --go-grpc_out=./gen/go --go-grpc_opt=paths=source_relative ./protos/*/*.proto
 echo "proto file create success ..."
--- gen-py3.sh
+++ gen-py3.sh
@@ -2,4 +2,5 @@ if [ ! -d "./gen/py3" ]; then
 	mkdir -p "./gen/py3"
 fi
 
-python3 -m grpc_tools.protoc -I./protos --python_out=./gen/py3 --grpc_python_out=./gen/py3 ./protos/*/*
+python3 -m grpc_tools.protoc --proto_path=. -I./protos --python_out=./gen/py3 --grpc_python_out=./gen/py3 ./protos/*/*.proto
+echo "proto file create success ..."
