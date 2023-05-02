9e01d002b8c4c39afabaefa97ba5be9894fcec05
ty <2805347171@qq.com>
2023-05-02 17:30:38 +0800

    fix: setup.py

--- gen/py3/setup.py
+++ gen/py3/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='py3_proto',
+setup(name='py3',
       version='v0.0.1', 
-      description='py3_proto', 
-      url='https://github.com/solost23/py3/proto', 
+      description='py3', 
+      url='https://github.com/solost23/protopb/gen/py3', 
       author='solost23',
       author_email='tianyuanyuans@163.com', 
-      packages=['python3_proto'], 
+      packages=['py3'], 
       )
