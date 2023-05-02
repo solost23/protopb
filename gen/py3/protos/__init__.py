0ee191a4635c1a166ed34d75a1fbdb2099869095
ty <2805347171@qq.com>
2023-05-02 17:21:31 +0800

    feat: python package

---
+++ gen/py3/protos/__init__.py
@@ -0,0 +1,5 @@
+from .common import *
+from .es import *
+from .order_machine import *
+from .oss import *
+from .push import *
---
+++ gen/py3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='py3_proto',
+      version='v0.0.1', 
+      description='py3_proto', 
+      url='https://github.com/solost23/py3/proto', 
+      author='solost23',
+      author_email='tianyuanyuans@163.com', 
+      packages=['python3_proto'], 
+      )
